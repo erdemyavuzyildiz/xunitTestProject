5e225501eadc08acdcf0904a8774ff41ace54c59
ErdemYavuzYildiz <erdemyavuzyildiz@hotmail.com>
2016-02-27 23:51:22 +0200

    initial

---
+++ UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Xunit;
+
+namespace UnitTestProject2
+{
+	public class UnitTest1 : IClassFixture<BaseClass>
+	{
+		[Fact]
+		public void PassingTest()
+		{
+			Assert.Equal(4, Add(2, 2));
+		}
+
+		[Fact]
+		public void FailingTest()
+		{
+			Assert.Equal(5, Add(2, 2));
+		}
+
+		private int Add(int x, int y)
+		{
+			return x + y;
+		}
+	}
+
+	public class BaseClass : IDisposable
+	{
+		public BaseClass()
+		{
+		}
+
+		public void Dispose()
+		{
+		}
+	}
+}
